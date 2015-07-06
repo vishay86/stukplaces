8b0927f6ec0b0b9b4143dedd4a1b504ec4ba0c7a
Vishay Bhatia <vishay@addictivegifts.com>
2015-07-06 12:09:50 +0400

    Added search to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -17,4 +17,12 @@ class Place < ActiveRecord::Base
 	rescue ZeroDivisionError
 		0
 	end
+
+	def self.search(search)
+		if search
+			where(['name LIKE ? OR address LIKE ?', "#{search}", "#{search}"])
+		else
+			all
+		end
+	end
 end
